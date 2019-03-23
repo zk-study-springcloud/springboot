25714d490d3224bfed53b7b2a4a0b8b347667310
zhaokai <506683059@qq.com>
2019-03-23 23:37:17 +0800

    Spring Boot集成Redis

--- src/test/java/com/zk/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/zk/springboot/SpringbootApplicationTests.java
@@ -1,7 +1,9 @@
 package com.zk.springboot;
 
+import com.zk.springboot.redis.RedisDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,16 @@ public class SpringbootApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Autowired
+    RedisDao redisDao;
+    @Test
+    public void testRedis() {
+        redisDao.setKey("name","forezp");
+        redisDao.setKey("age","17");
+//        System.out.println(redisDao.getValue("name"));
+//        System.out.println(redisDao.getValue("age"));
     }
 
 }
